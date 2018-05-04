381d148aa455747a6d07ad3d103fb2a8dad7fbd6
José Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-05-03 16:47:17 -0800

    Subida al servidor ubuntu por filezila

--- src/index.js
+++ src/index.js
@@ -15,39 +15,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 3000);
 
-//app.use( function (req, res, next) {
-    //en vez de * se puede definir SÓLO los orígenes que permitimos
-  //  res.header('Access-Control-Allow-Origin', "*");
-    //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //metodos http permitidos para CORS
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin',"*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers','Content-Type');
+    next();
+});
 
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //next();
-//})
-
-
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
+// app.use( function (req, res, next) {
+//     //en vez de * se puede definir SÓLO los orígenes que permitimos
+//    res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     //metodos http permitidos para CORS
+//
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     /*Linea agregada para que aceptara el metodo PUT*/
-//     res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, PATCH, DELETE");
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
 //     next();
 // });
-
-function perimitirCrossDomain(req, res, next) {
-    //en vez de * se puede definir SÓLO los orígenes que permitimos
-    res.header('Access-Control-Allow-Origin', '*');
-    //metodos http permitidos para CORS
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-app.listen(app.get('port'), function () {
-    console.log('Servidor iniciado ', app.get('port'));
-});
 
 app.get('/', function (req, res) {
     return res.send('<h1>Titulo 1</h1><p>Este es un parrafo 1</p>')
@@ -65,8 +49,7 @@ app.get('/API/usuarios', function (req, res) {
 
 /*Para insertar un nuevo usuario*/
 app.post('/API/usuarios', function (req, res) {
-
-    let persona = req.body;
+    var persona = req.body;
     if(!persona) {
        return res.status(400).send({error: true, message: 'Por favor ingrese un usuario'});
    }
@@ -77,15 +60,44 @@ app.post('/API/usuarios', function (req, res) {
    });
 });
 
-/*Para eliminar*/
-app.delete('/API/usuarios', function (req, res) {
-    let id = req.body;
-    console.log(id);
-    if(!id)
-        return res.status(400).send({error: true, message: 'No existe id'});
-    conection.query('delete from prueba_persona where id = ?',[id], function (error, result, fields) {
-        if(error)
-            throw error;
-        return res.send({error: false, data: result, message: 'Registro eliminado exitosamente'});
+
+app.put('/API/usuarios', function (req, res) {
+    var usuario = req.body;
+
+    if(!usuario.idprueba) {
+        res.status(409);
+        res.send("No exite valor a modificar");
+    }
+    conection.query("UPDATE prueba_persona SET edad = ? WHERE idprueba = ?", [usuario.edad, usuario.idprueba], function (error, results, fields) {
+        if (error) throw error;
+            res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
     });
+    // res.status(404);
+    // res.send("Id no encontrado");
+});
+
+
+app.delete('/API/usuarios', function (req, res) {
+    var id = req.body;
+    console.log('el id es: ');
+    console.log(id);
+    if(!id) {
+        res.status(409);
+        res.send("No exite valor a modificar");
+    }
+    conection.query("delete from prueba_persona WHERE idprueba = ?", [id], function (error, results, fields) {
+        if (error) throw error;
+        res.send({ error: false, data: results, message: 'Usuario eliminado exitosamente' });
+    });
+    // res.status(404);
+    // res.send("Id no encontrado");
+});
+
+app.all('*', function (req, res, next) {
+    res.send('<h1>Pagina no encontrada</h1>')
+})
+
+
+app.listen(app.get('port'), function () {
+    console.log('Servidor iniciado ', app.get('port'));
 });
